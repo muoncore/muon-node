0402e325ff054d578ee359727b54698d46947c93
Archonix <archonix@users.noreply.github.com>
2014-12-12 00:45:12 +0000

    Updated for publish to NPM

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "muon": require('./muon/muon-core.js'),
+    "amqpTransport": require("./muon/muon-transport-amqp.js"),
+    "httpTransport": require("./muon/muon-transport-http.js")
+};
