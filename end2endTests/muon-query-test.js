be6632086d745637474f79dc1d2e79510734b7e7
Gawain Hammond <gawain@Techs-Mac-mini.local>
2015-07-09 16:21:09 +0100

    added assertions

--- end2endTests/muon-query-test.js
+++ end2endTests/muon-query-test.js
@@ -1,8 +1,9 @@
-
 var _ = require("underscore");
 var sleep = require('sleep');
 var muonCore = require("../index.js");
 var sleep = require('sleep');
+var expect = require('expect.js');
+var assert = require('assert');
 
 var amqp = muonCore.amqpTransport("amqp://muon:microservices@msg.cistechfutures.net:5672");
 
@@ -12,22 +13,47 @@ var muonServer = muonCore.muon('node-service', amqp.getDiscovery(), [
 
 muonServer.addTransport(amqp);
 
+var params = {'name': 'bob', 'email': 'bob@sky.com'};
+var serverMessage = 'muon node server';
 
-setTimeout(function() {
-    muonServer.resource.onQuery("/query", "Get the events", function(event, data, respond) {
-            console.log('muon node-service server onQuery("/query"): event:', event);
-            respond({"echo":"this is the server response"});
-        });
-},3500);
+describe("Simple muon resource client/server test", function () {
 
-//sleep.sleep(3);
+    this.timeout(7000);
 
-setTimeout(function() {
-    muonServer.resource.query("muon://node-service/query?arg1=foobar&arg2=soa", function(event, payload) {
-            console.log('muon node-service client: response event:', event);
-            console.log(payload);
-        });
-},3500);
+    before(function() {
+        startServer();
+    });
+
+    it("server echoes back client query parameters", function (done) {
+
+        setTimeout(function() {
+            muonServer.resource.query('muon://node-service/query?name=' + params.name + '&email=' + params.email, function(event, payload) {
+                    console.log('muon node-service client: response event: ', event);
+                    console.log('muon node-service client: response payload :',payload);
+
+                        assert.equal(payload.message, serverMessage.a, 'server response message');
+                        assert.equal(payload.params.name, params.name, 'server response message');
+                        assert.equal(payload.params.email, params.email, 'server response message');
+                        done();
+
+                });
+        },2000);
+
+
+    });
+
+});
+
+
+function startServer() {
+    setTimeout(function() {
+        muonServer.resource.onQuery("/query", "Get the events", function(event, data, respond) {
+                console.log('muon node-service server onQuery("/query"): event:', event);
+                respond({'message': serverMessage, 'params': event.headers.qparams});
+            });
+    },2000);
+
+}
 
 
 
