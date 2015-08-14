0b15232f79c0cd46fecb777093b37f547584042d
JBH92 <j.bartholomew@outlook.com>
2015-08-14 11:18:04 +0100

    added new end2endtest to check that projections are returned correctly

---
+++ end2endTests/projection-query-test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+
+var muonCore = require("../index.js");
+
+it('check that projection-keys query to eventstore returns array of keys', function(done) {
+  this.timeout(7000);
+
+  var amqp = muonCore.amqpTransport('amqp://muon:microservices@localhost:5672');
+
+  //Define muon instance for the communications to use
+  var muonSystem = muonCore.muon('tagcloud', amqp.getDiscovery(), [
+      ["my-tag", "tck-service", "node-service"]
+  ]);
+
+  //Connect transport stream to the instance
+  muonSystem.addTransport(amqp);
+
+  muonSystem.resource.query('muon://photon/projection-keys', function(event, payload) {
+    console.log(event);
+    console.log(payload);
+    var keys = payload.data['projection-keys'];
+    assert(Array.isArray(keys));
+
+    done();
+  });
+
+});
