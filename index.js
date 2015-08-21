554371dea7bb4bc319af9ac94c42a5d53b04f0b1
David Dawson <david.dawson@simplicityitself.com>
2015-08-21 17:55:00 +0100

    Cleanup dead files.

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ var Logger = require('./lib/logging/logger');
 module.exports = {
     "muon": require('./core/muon-core.js'),
     "amqpTransport": require("./amqp/muon-transport-amqp.js"),
-    "httpTransport": require("./http/muon-transport-http.js"),
-    "dummyTransport": require("./core/muon-transport-dummy.js")
+    "httpTransport": require("./http/muon-transport-http.js")
 };
