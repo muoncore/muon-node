7d53e1167b54756745b61ed174a52d02eb2b351b
David Dawson <david.dawson@simplicityitself.com>
2016-01-26 13:14:54 +0000

    Functional amqp transport (client portion)

--- muon/transport/amqp/amqp09-transport.js
+++ muon/transport/amqp/amqp09-transport.js
@@ -1,11 +1,99 @@
+var _ = require("underscore");
+var bichannel = require('../../channel/bi-channel');
 
+var AmqpConnection = require("./infra/amqp-connection");
+var AmqpQueue = require("./infra/amqp-queues");
 
-var Amqp09Transport = function () {
+var Amqp09Transport = function (url) {
+    this.connection = new AmqpConnection(url);
+    this.connection.connect(function() {
+        this.queues = new AmqpQueue(this.connection);
+    }.bind(this));
 };
 
 Amqp09Transport.prototype.openChannel = function(serviceName, protocolName) {
 
+    var transport = this;
+
+    var channelConnection = {
+        serviceName: serviceName,
+        protocolName: protocolName,
+        channelOpen:false,
+        outboundBuffer:[],
+        drainQueue: function() {
+
+            _.each(this.outboundBuffer, function(el) {
+                this.send(el);
+            }.bind(this));
+            console.info("send " + this.outboundBuffer.length + " pending messages");
+            this.outboundBuffer = [];
+        },
+        shutdown: function() {
+            logger.info("CHANNEL POISONED");
+        },
+        send: function(msg) {
+
+            var amqpMessage = {
+                headers: msg.headers,
+                payload:msg.payload
+            };
+
+            logger.info("SENDING MESSAGE!");
+            console.dir(msg);
+
+
+
+            transport.queues.send(this.sendQueue, amqpMessage);
+        }
+    };
+
+    channelConnection.channel = bichannel.create("test-channel");
+
+    channelConnection.channel.right().listen(function(msg) {
+        logger.info("Sending message");
+        if (msg == "poison") {
+            channelConnection.shutdown();
+            return;
+        }
+        console.dir(msg);
+        if(channelConnection.channelOpen) {
+            channelConnection.send(msg);
+        } else {
+            channelConnection.outboundBuffer.push(msg);
+        }
+    }.bind(channelConnection));
+
+    this.startHandshake(channelConnection);
+
+    return channelConnection.channel.left();
 };
 
-module.exports = AmqpTransport;
+Amqp09Transport.prototype.startHandshake = function(channelConnection) {
+    channelConnection.sendQueue = "node-service-send";
+    channelConnection.receiveQueue = "node-service-recieve";
+
+    this.queues.listen(channelConnection.receiveQueue, function(message) {
+        logger.info("GOT DATA ON RECEIVE QUEUE!");
+        console.dir(message);
+        if (channelConnection.channelOpen == false) {
+            channelConnection.drainQueue();
+            channelConnection.channelOpen = true;
+        }
+        channelConnection.channel.right().send(message);
+    });
+
+    var serviceQueueName = "service." + channelConnection.serviceName;
+
+    this.queues.send(serviceQueueName, {
+        headers: {
+            PROTOCOL:channelConnection.protocolName,
+            SOURCE_SERVICE:"",
+            REPLY_TO:channelConnection.receiveQueue,
+            LISTEN_ON:channelConnection.sendQueue
+        }
+    })
+};
+
+
+module.exports = Amqp09Transport;
 
--- tester.js
+++ tester.js
@@ -1,10 +1,43 @@
-
 require("sexylog");
 var AmqpDiscovery = require("./muon/discovery/amqp/amqp-discovery.js");
-
+var AmqpTransport = require("./muon/transport/amqp/amqp09-transport");
 
 var discovery = new AmqpDiscovery("amqp://muon:microservices@localhost");
 discovery.advertiseLocalService({
     identifier:"awesome",
-    tags:["node", "awesome"]
+    tags:["node", "awesome"],
+    codecs:["application/json"]
 });
+
+var transport = new AmqpTransport("amqp://muon:microservices@localhost");
+
+setTimeout(function() {
+
+    var connection = transport.openChannel("ExampleService", "request");
+
+    connection.listen(function(response) {
+        logger.info("The channel replied");
+        console.dir(response.payload);
+    });
+
+    connection.send({
+        headers:{
+            eventType:"RequestMade",
+            id:"simples",
+            targetService:"ExampleService",
+            sourceService:"awesome",
+            protocol:"request",
+            url:"/",
+            "Content-Type":"application/json",
+            sourceAvailableContentTypes:["application/json"],
+            channelOperation:"NORMAL"
+        },
+        payload:{
+            be:"happy"
+        }});
+
+    setTimeout(function() {
+        connection.send("poison");
+    }, 7000);
+
+}, 4500);
