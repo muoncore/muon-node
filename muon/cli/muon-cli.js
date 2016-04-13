887989e97b89ee1bf3313fa9f3168ed5fe909502
gawain hammond <spam.gawain@gmail.com>
2016-04-13 22:54:57 +0100

    added cli error help message

--- muon/cli/muon-cli.js
+++ muon/cli/muon-cli.js
@@ -2,6 +2,11 @@
 
 var cli = require('cli').enable('status');
 require('sexylog');
+var url = require('url');
+var Joi = require('joi');
+
+
+
 
 var amqpUrl = process.env.MUON_URL;
 
@@ -14,7 +19,12 @@ cli.parse({
  
 cli.main(function(args, options) {
 
- 
+    var validAmqpUrl =  Joi.validate(amqpUrl,  Joi.string().uri().required());
+
+    if (validAmqpUrl.error) {
+        logger.error('amqp env variable url invalid! MUON_URL=' + amqpUrl);
+        exit();
+    }
     this.ok('muon cli connected: ' + amqpUrl);
  
     this.ok('options: ' + JSON.stringify(options));
@@ -41,7 +51,15 @@ function discover() {
 
 }
 
+var validate = function(err, value) {
+    logger.info(err);
 
+
+    if (err) {
+        logger.error(value);
+        exit();
+    }
+}
 
 function exit() {
     process.exit(0);
