61fa08e5e31e956867b063df011caabad9545c88
David Dawson <david.dawson@simplicityitself.com>
2016-05-02 12:12:04 +0100

    Updates for muonjs integration.
    Expose more APIs to enable configuration.

--- muon/api/muoncore.js
+++ muon/api/muoncore.js
@@ -10,29 +10,46 @@ var messages = require('../domain/messages.js');
 var ServerStacks = require("../../muon/api/server-stacks");
 var amqpTransport = require('../../muon/transport/amqp/transport.js');
 var builder = require("../infrastructure/builder");
-
+var channel = require("../infrastructure/channel")
 
 
 exports.create = function(serviceName, transportUrl, discoveryUrl) {
 
     var config = builder.config(serviceName, transportUrl, discoveryUrl);
 
-    var infrastructure = new builder.build(config, rpcApi);
+    var infrastructure = new builder.build(config);
+    
+    return exports.api(serviceName, infrastructure)
+}
+
+exports.channel = function() {
+    return channel;
+}
+
+exports.ServerStacks=ServerStacks
+
+exports.api = function(
+    serviceName,
+    infrastructure
+) {
     var rpcApi = rpc.getApi(serviceName, infrastructure.transport);
     infrastructure.serverStacks.addProtocol(rpcApi);
 
-    var muonApi = {
-        discovery: function() { return infrastructure.discovery },
-        shutdown: function() {
+    return {
+        infrastructure: function() { return infrastructure },
+        transportClient: function() { return infrastructure.transport },
+        discovery: function () {
+            return infrastructure.discovery
+        },
+        shutdown: function () {
             logger.warn("Shutting down muon!");
             infrastructure.shutdown();
         },
-        request: function(remoteServiceUrl, data, clientCallback) {
+        request: function (remoteServiceUrl, data, clientCallback) {
             return rpcApi.request(remoteServiceUrl, data, clientCallback);
         },
-        handle: function(endpoint, callback) {
-             rpcApi.handle(endpoint, callback);
+        handle: function (endpoint, callback) {
+            rpcApi.handle(endpoint, callback);
         }
     };
-    return muonApi;
 }
