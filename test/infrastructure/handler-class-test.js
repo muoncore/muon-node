9834e6f85b5ae6f6a638a20abe7bbc51bf6bde09
gawain hammond <spam.gawain@gmail.com>
2016-06-05 22:54:59 +0100

    added test for function mixup with handler as class

--- test/infrastructure/handler-class-test.js
+++ test/infrastructure/handler-class-test.js
@@ -63,6 +63,98 @@ describe("Handler class test:", function () {
 
     });
 
+    it("outgoing handler between two channels sends back", function (done) {
+
+            var msg = {count: 0, audit: []};
+
+              var upstream = bichannel.create("upstream");
+              var downstream = bichannel.create("downstream");
+
+               class TestHandler extends Handler {
+
+                 outgoingFunction(message, forward, back, route) {
+                   message.count++;
+                   message.audit.push('testHandler.outgoing()');
+                   back(message);
+                 }
+
+                 incomingFunction(message, forward, back, route) {
+                   message.count++;
+                   message.audit.push('testHandler.incoming()');
+                   forward(message);
+                 }
+               }
+
+               var testHandler = new TestHandler('test');
+
+                upstream.rightHandler(testHandler);
+                downstream.leftHandler(testHandler);
+
+                upstream.leftSend(msg);
+
+                upstream.leftConnection().listen(function(message) {
+                        console.log('***** upstream message returned:');
+                        console.dir(message);
+                        assert.equal(message.count, 1);
+                        assert.equal(message.audit[0], 'testHandler.outgoing()');
+                        done();
+                });
+
+
+                //downstream.rightConnection().listen(function(message){
+                //    downstream.rightSend(message);
+                //});
+
+    });
+
+
+    it("incoming handler between two channels sends back", function (done) {
+
+            var msg = {count: 0, audit: []};
+
+              var upstream = bichannel.create("upstream");
+              var downstream = bichannel.create("downstream");
+
+               class TestHandler extends Handler {
+
+                 outgoingFunction(message, forward, back, route) {
+                   message.count++;
+                   message.audit.push('testHandler.outgoing()');
+                   forward(message);
+                 }
+
+                 incomingFunction(message, forward, back, route) {
+                   message.count++;
+                   message.audit.push('testHandler.incoming()');
+                   back(message);
+                 }
+               }
+
+               var testHandler = new TestHandler('test');
+
+                upstream.rightHandler(testHandler);
+                downstream.leftHandler(testHandler);
+
+                upstream.leftSend(msg);
+
+                var downstreamCalls = 0;
+                downstream.rightConnection().listen(function(message) {
+                    downstreamCalls++;
+                    if (downstreamCalls == 1) {
+                        downstream.rightSend(message);
+                    } else {
+                      console.log('***** downstream message returned:');
+                      console.dir(message);
+                      assert.equal(message.count, 2);
+                      assert.equal(message.audit[0], 'testHandler.outgoing()');
+                      assert.equal(message.audit[1], 'testHandler.incoming()');
+                      done();
+                    }
+
+                });
+
+    });
+
     it("single channel handler routes inbound message to callback and return response outbound", function (done) {
 
             var msg = {count: 0, audit: [], routingKey: 'server'};
