d70bf73d8a69a8cdc613553d1f9595fc521db65e
Gawain Hammond <spam.gawain@gmail.com>
2016-06-10 15:07:40 +0100

    added keep alive test

--- muon/socket/agent.js
+++ muon/socket/agent.js
@@ -2,15 +2,21 @@
 require('sexylog');
 var moment = require('moment');
 
+/**
+  Muon SOcket Kep Alive Agent
+
+  Sends ping messages to other socket host to ensure connection stays up
+*/
+
 class MuonSocketAgent {
 
-  constructor(upstreamChannel, downstreamChannel, protocol, repeatMs) {
+  constructor(upstreamChannel, downstreamChannel, protocol, offsetMs) {
 
     this.upstreamChannel = upstreamChannel;
     this.downstreamChannel = downstreamChannel;
 
-    if (! repeatMs) repeatMs = 0;
-    this.repeatMs = repeatMs;
+    if (! offsetMs) offsetMs = 0;
+    this.offsetMs = offsetMs;
     this.lastMessageTimestamp = new Date();
     var _outboundFunction = this.outbound; //'this' doesnt work in functions below
     var _inboundFunction = this.inbound; //'this' doesnt work in functions below
@@ -25,21 +31,22 @@ class MuonSocketAgent {
     });
 
     var keepAlive =   function() {
-        if (messageSentSince(this.lastMessageTimestamp, this.repeatMs)) return;
+        if (messageWasSentSince(this.lastMessageTimestamp, this.offsetMs)) return;
         var ping = {
           protocol:protocol,
           step: "keep-alive",
           payload: {}
         }
-        logger.debug('[*** MUON:SOCKET:AGENT:OUTBOUND ***] send keep alive ping');
+        logger.trace('[*** MUON:SOCKET:AGENT:OUTBOUND ***] sending keep alive ping');
         this.downstreamChannel.leftConnection().send(ping);
 
       }.bind(this);
-      if (this.repeatMs > 0) {
+
+      if (this.offsetMs > 0) {
         setInterval(function() {
           //logger.trace('[*** MUON:SOCKET:AGENT:OUTBOUND ***] setInterval(f(){}, ' + this.repeatMs + 'ms)');
           keepAlive();
-        }.bind(this), this.repeatMs);
+        }.bind(this), this.offsetMs);
       }
 
   }
@@ -53,20 +60,20 @@ class MuonSocketAgent {
       logger.trace('[*** MUON:SOCKET:AGENT:INBOUND ***]');
       upstreamConnection.send(message);
   }
-
+/*
   setLastMessageTimestamp() {
       //console.log('setLastMessageTimestamp()', this);
       this.lastMessageTimestamp = new Date();
       logger.trace('this.lastMessageTimestamp=' + this.lastMessageTimestamp);
   }
-
+*/
 
 
 }
 
 
-function messageSentSince(lastMessageTimepstamp, ms) {
-  var moment1 = moment(lastMessageTimepstamp).add(ms, 'milliseconds');
+function messageWasSentSince(lastMessageTimepstamp, offsetMs) {
+  var moment1 = moment(lastMessageTimepstamp).add(offsetMs, 'milliseconds');
   var moment2 = moment(new Date());
   //logger.trace('moment1/moment2: ' + moment1 + "/" + moment2);
   var inTimeWindow = (moment2).isBefore(moment1) ;
--- test/socket/agent-test.js
+++ test/socket/agent-test.js
@@ -38,16 +38,50 @@ describe("Agent class test:", function () {
 
               var upstream = bichannel.create("upstream");
               var downstream = bichannel.create("downstream");
-
-              var agent = new Agent(upstream, downstream, 'rpc', 10);
+              var protocol = 'rpc';
+              var agent = new Agent(upstream, downstream, protocol, 10);
 
                 var keepAlivePingCount = 0;
                 downstream.rightConnection().listen(function(message){
                     keepAlivePingCount++;
-                    if (keepAlivePingCount == 3) done();
+
+                    assert.equal(protocol, message.protocol);
+                    assert.equal('keep-alive', message.step);
+                      if (keepAlivePingCount == 3) done();
                 });
 
     });
+
+    /**  SEND 10 message (once every 10ms, then wait for keep alive)   */
+    it("agent delays keep alive pings if event received", function (done) {
+
+              var upstream = bichannel.create("upstream");
+              var downstream = bichannel.create("downstream");
+              var protocol = 'rpc';
+              var agent = new Agent(upstream, downstream, protocol, 200);
+
+              var messagesSent = 0;
+              var backgroundSender = setInterval(function() {
+                upstream.leftConnection().send({text: 'this is a test event!'});
+                messagesSent++;
+                if (messagesSent == 10) {
+                  clearInterval(backgroundSender);
+                }
+              }, 10);
+
+                var pingReceived = false;
+                var messagesReceived = 0;
+                downstream.rightConnection().listen(function(message){
+                    //console.log('******************** message: ', message);
+                    messagesReceived++;
+                    if (message.step == 'keep-alive') pingReceived = true;
+                    if (message.text) assert.equal('this is a test event!', message.text);
+                    if (messagesReceived > 10 && pingReceived) done();
+                });
+
+    });
+
+    
 
 
 
