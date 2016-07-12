497b0ec6ef28132e91056670ce0b59551d5efaa0
gawain hammond <spam.gawain@gmail.com>
2016-07-12 21:41:18 +0100

    added amqp queue delete to muon socket client

--- test/transport/amqp/client-test.js
+++ test/transport/amqp/client-test.js
@@ -6,6 +6,7 @@ var expect = require('expect.js');
 var uuid = require('node-uuid');
 var messages = require('../../../muon/domain/messages.js');
 var amqp = require('../../../muon/transport/amqp/amqp-api.js');
+var amqplib = require('amqplib/callback_api');
 
 var AmqpDiscovery = require("../../../muon/discovery/amqp/discovery");
 
@@ -44,8 +45,9 @@ describe("client test:", function () {
         var serverName = 'serverabc123';
         var url = "amqp://muon:microservices@localhost";
         var discovery = new AmqpDiscovery(url);
+        var fakeAmqpApi = {};
 
-        var muonClientChannel = client.connect(serverName, 'rpc', url, discovery);
+        var muonClientChannel = client.connect(serverName, 'rpc', fakeAmqpApi, discovery);
 
         muonClientChannel.listen(function (msg) {
             expect(msg.status).to.contain('failure');
@@ -54,5 +56,41 @@ describe("client test:", function () {
             done();
         });
     });
+
+
+      it("client deletes muon socket queues on channel_op equals closed message", function (done) {
+
+        var serverName = 'serverabc123';
+        var url = "amqp://muon:microservices@localhost";
+
+        var deleteCalled = 0;
+        var discovery = {
+            discoverServices: function(cb) {
+                var services = {find: function() {return {identifier: serverName}}};
+                cb(services);
+            },
+            serviceList: [{identifier: serverName}]
+        }
+        var amqpApi = {
+          delete: function(queueName) {
+              deleteCalled++;
+              expect(queueName).to.contain(serverName);
+              if (deleteCalled == 2) done(); // it's been called twice
+          },
+          outbound: function() {
+            return {send: function(){}};
+          },
+          inbound: function() {
+            return {listen: function(cb) {cb({headers: {handshake: 'accepted'}})}}
+          }
+        };
+
+        var muonClientChannel = client.connect(serverName, 'rpc', amqpApi, discovery);
+
+        muonClientChannel.send(messages.shutdownMessage());
+
+
+
+      });
 
 });
